c4e7df058948de17646a860b2cad1d002cbdb205
Nisarg <emailtonisarg@gmail.com>
2020-08-15 16:09:40 -0400

    🐛 Converted to GQL Friendly Query

--- index.php
+++ index.php
@@ -16,23 +16,18 @@
       curl_setopt($ch, CURLOPT_POST, 1);
       $headers = [
         'Content-Type: application/json',
+        'comminfo: {"session":{"uid":0,"openid":""},"session_ext":{"token_type":2,"token":"","token_channel":0},"app_info":{"version_code":0,"version_name":"","platform":4,"terminal_type":2,"pvid":"781983539220081405","language":"en-US","client_type":4},"client_info":{"device_info":"{"tid":"15973856294109155"}"}}'
       ];
       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
       curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); // Do not send to screen
-      $obj1->operationName = "getLiveInfo";
+      $obj1->query = 'query getLiveInfo($params: GetLiveInfoReqInput){ getLiveInfo(params: $params){ streamerInfo{uid userName} programInfo{streamInfo{bitrate playUrl}} } }';
       $obj1->variables->params->userName = $_GET['stream'];
-      $obj1->variables->params->requireDecorations = true;
-      
-      $obj1->extensions->persistedQuery->version = 1;
-      $obj1->extensions->persistedQuery->sha256Hash = "7aa67414a6cbc0f23155bad666ab31561f6b11bf6deda4025c7ce805e5a7f187";
-      $obj = [$obj1];
-      $post_data = json_encode($obj);
-      //echo $post_data;
+      $post_data = json_encode($obj1);
       curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
       $apiResponse = curl_exec($ch);
       curl_close($ch);
       $jsonArrayResponse = json_decode($apiResponse);
-      $url = $jsonArrayResponse[0]->data->getLiveInfo->programInfo->streamInfo[1]->playUrl;
+      $url = $jsonArrayResponse->data->getLiveInfo->programInfo->streamInfo[1]->playUrl;
     }
   ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flv.js/1.5.0/flv.min.js"></script>
